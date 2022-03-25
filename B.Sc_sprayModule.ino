d75913340888f3606f89ecc43d80393099013748
Tollak Liland <91386549+tollakl@users.noreply.github.com>
2022-03-25 14:15:27 +0100

    simple weigt with vesc

--- B.Sc_sprayModule.ino
+++ B.Sc_sprayModule.ino
@@ -35,248 +35,298 @@ VL53L0X sensor;
 Servo actuator;
 Servo ESC;
 
-#define KP_POT A0
-#define KI_POT A1
-#define KD_POT A2
 #define RED_BUTTON 2
-#define EN_A 9
-#define EN_B 8
-#define PWM 6
 
 
-int kp_analog = 0;
-int ki_analog = 0;
-int kd_analog = 0;
 
-
+bool regulate = false;
 int lastButtonState;
 int currentButtonState;
-bool regulate = false;
 
-float e;
-float pos = 0;
 long prevT = 0;
-
 float eprev = 0;
 float eintegral = 0;
 
 bool spray = false;
 int counter = 0;
-
- 
+float roll = 0;
 
 void setup()
 {
-	Serial.begin(115200);
-  pinMode(PWM ,OUTPUT);
-  pinMode(EN_A ,OUTPUT);
-  pinMode(EN_B ,OUTPUT);
-	pinMode(RED_BUTTON, INPUT_PULLUP);
+  Serial.begin(115200);
 
-	Wire.begin();
+  pinMode(RED_BUTTON, INPUT_PULLUP);
 
-	if (myIMU.begin() == false)
-	{
-		//Serial.println(F("BNO080 not detected at default I2C address"));
-		while (1);
-  	}
+  Wire.begin();
+  myIMU.begin();
 
-	//Init I2C and IMU
   
-	//Wire.setClock(400000); //Increase I2C data rate to 400kHz
-	myIMU.enableRotationVector(50); //Send data update every 50ms
+  if (myIMU.begin() == false)
+  {
+    Serial.println(F("BNO080 not detected at default I2C address"));
+    while (1);
+    }
 
-	//Serial.println(F("Rotation vector enabled"));
-	//Serial.println(F("Output in form roll, pitch, yaw"));
+  //Init I2C and IMU
+  Wire.setClock(400000); //Increase I2C data rate to 400kHz
+  myIMU.enableRotationVector(50); //Send data update every 50ms
+  
+  //Serial.println(F("Rotation vector enabled"));
+  //Serial.println(F("Output in form roll, pitch, yaw"));
 
 
-	//Time of Flight sensor
-	sensor.init();
-	sensor.setTimeout(500);
+  //Time of Flight sensor
+  sensor.init();
+  sensor.setTimeout(500);
 
-  //Actuate spray gun
+
+
   actuator.attach(5);
-	TCCR0B = (TCCR0B & 0b11111000) | 0x02;
-    
+  TCCR0B = (TCCR0B & 0b11111000) | 0x02;
+
+  ESC.attach(11);
+  ESC.write(90);
+  delay(500);
 }
+
 
 void loop()
 {
-    
-  // Read analog potentiometer values
-    kp_analog = analogRead(KP_POT);
-    ki_analog = analogRead(KI_POT);
-    kd_analog = analogRead(KD_POT);
-
-	// Map the analog potentiometer values to matching PID values
-    float kp = map(kp_analog, 0, 1023, 0, 1000);
-    float ki = map(ki_analog, 0, 1023, 0, 1000);
-    float kd = map(kd_analog, 0, 1023, 0, 1000);
-
-	kp = (kp/100)*10;
-	ki = (ki/100)*1;
-	kd = (kd/100)*1;
-
-	//Serial.print("Kp: ");
-	//Serial.print(kp, 4);
-	//Serial.print(", ");
-	//Serial.print("Ki: ");
-	//Serial.print(ki, 4);
-	//Serial.print(", ");
-	//Serial.print("Kd: ");
-	//Serial.print(kd, 4);
-  //Serial.print(", ");
-  //Serial.print(" - Control Mode: ");
-
-	if (regulate)
-	{
-		//Serial.print("PID - Position: ");
-	}
-	else
-	{
-		//Serial.println(" IDLE");
-    //runMotor(1,0,PWM,EN_A,EN_B);
-	}
-
-	  lastButtonState    = currentButtonState;
+    lastButtonState    = currentButtonState;
     currentButtonState = digitalRead(RED_BUTTON);
 
-	// If the button is pressed, toggle the on/off the control loop
-	if(lastButtonState == HIGH && currentButtonState == LOW) 
-	{
+  // If the button is pressed, toggle the on/off the control loop
+  if(lastButtonState == HIGH && currentButtonState == LOW) {
     regulate = !regulate;
     }
 
-	// If the regulate bool is true, then start PID
-	if (regulate)
-	{
+  // If the regulate bool is true, then start PID
+  if (regulate)
+  {
+    //Look for reports from the IMU
+    if (myIMU.dataAvailable() == true)
+    {   
     actuate();
+    
+    //Get data from IMU
+    float roll = (myIMU.getRoll()) * 180.0 / PI; // Convert roll to degrees
+    //float pitch = (myIMU.getPitch()) * 180.0 / PI; // Convert pitch to degrees
+    //float yaw = (myIMU.getYaw()) * 180.0 / PI; // Convert yaw / heading to degrees
+    
+    
+    
+    //float roll = myIMU.getLinAccelY();
+    //float y = myIMU.getLinAccelY();
+    //float z = myIMU.getLinAccelZ();
+
+
+    
+    //Get data from TOF
+    int distance = sensor.readRangeSingleMillimeters();
+    
+
+
+
+    if (sensor.timeoutOccurred()) { Serial.print(" TIMEOUT"); }
+
+    //-----PID control-----
+
+    // set target position
+    float target = -2.03;
+    //int target = 250*sin(prevT/1e6);
+
+    //PID constants (commented out due to potentiometer implementation)
+       float kp = 20;
+       float kd = 0;
+       float ki = 0;
+
+    //distanc from wall
+    float pos = -roll;
+    
+    // error
+    float e = pos-target;
+
+    // time difference
+    long currT = micros();
+    float deltaT = ((float) (currT - prevT))/( 1.0e6 );
+    prevT = currT;
+    
+    //derivative
+    float dedt = (e-eprev)/(deltaT);
+
+    // integral
+    eintegral = eintegral + e*deltaT;
+
+    //control signal
+    float u = kp*e + kd*dedt + ki*eintegral;
+
+    int pwm = (int)fabs(-u+89);
+
+    //if(pwm < 89 )
+    //{
+    //  pwr = pwr-2;
+    //}
+
+//    if(pwr > 91 )
+//    {
+//      pwr = 91;
+//    }
+
+//    if(pwr < 85 )
+//    {
+//      pwr = 85;
+//    }
+
+//    if(e<0.05 || e>-0.05){
+//      pwr = 97;
+//    }
+    
+
+    //SEND PWM
+   
+    
+    ESC.write(pwm);
+
+    // store previous error
+    eprev = e;
+
+    //Serial.print(kp,4);
+    //Serial.print(", ");
+    Serial.print("IMU deg: ");
+    Serial.print(pos*10+50);
+    Serial.print(", ");
+    //Serial.print(millis());
+    Serial.print("Error: ");
+    Serial.print(e);
+    Serial.print(", ");
+    Serial.print("U: ");
+    Serial.print(u);
+    Serial.print(", ");
+    Serial.print("PWM: ");
+    Serial.println(pwm);
+    
+    }
+    
+    
   }
   else{
       disengage();
-  }
-  
-	  //Look for reports from the IMU
-		if (myIMU.dataAvailable() == true)
-		{		
-		
-			
-		//Get data from IMU
-		float roll = (myIMU.getRoll()) * 180.0 / PI; // Convert roll to degrees
-		//float pitch = (myIMU.getPitch()) * 180.0 / PI; // Convert pitch to degrees
-		//float yaw = (myIMU.getYaw()) * 180.0 / PI; // Convert yaw / heading to degrees
-    float getLinAccelX();
+      //Look for reports from the IMU
+    if (myIMU.dataAvailable() == true)
+    {   
+    
+    //Get data from IMU
+    float roll = (myIMU.getRoll()) * 180.0 / PI; // Convert roll to degrees
+    //float pitch = (myIMU.getPitch()) * 180.0 / PI; // Convert pitch to degrees
+    //float yaw = (myIMU.getYaw()) * 180.0 / PI; // Convert yaw / heading to degrees
+    
+    
+    
+    //float roll = myIMU.getLinAccelY();
+    //float y = myIMU.getLinAccelY();
+    //float z = myIMU.getLinAccelZ();
 
-		//Get data from TOF
-		//int distance = -sensor.readRangeSingleMillimeters();
+
+    
+    //Get data from TOF
+    int distance = sensor.readRangeSingleMillimeters();
     
 
-		//if (sensor.timeoutOccurred()) { Serial.print(" TIMEOUT"); }
 
 
-		//-----PID control-----
+    if (sensor.timeoutOccurred()) { Serial.print(" TIMEOUT"); }
 
-		// set target position
-		int target = 1.20;
-		//int target = 250*sin(prevT/1e6);
+    //-----PID control-----
 
-		//PID constants (commented out due to potentiometer implementation)
-		//   float kp = 1;
-		//   float kd = 0;
-		//   float ki = 0;
+    // set target position
+    float target = -2.03;
+    //int target = 250*sin(prevT/1e6);
 
-		//feedback data distance or angle
-		float pos = roll;
-		
-		// error
-		float e = (pos-target);
+    //PID constants (commented out due to potentiometer implementation)
+       float kp = 10;
+       float kd = 0;
+       float ki = 0;
 
-		// time difference
-		long currT = micros();
-		float deltaT = ((float) (currT - prevT))/( 1.0e6 );
-		prevT = currT;
-		
-		//derivative
-		float dedt = (e-eprev)/(deltaT);
-
-		// integral
-		eintegral = eintegral + e*deltaT;
-
-		//control signal
-		float u = kp*e + kd*dedt + ki*eintegral;
-
-    // motor power
-    int pwm = fabs((int)u);
-   
+    //distanc from wall
+    float pos = -roll;
     
-//    if(pwm < 110){
-//    digitalWrite(EN_A, LOW);
-//    digitalWrite(EN_B, LOW);
+    // error
+    float e = pos-target;
+
+    // time difference
+    long currT = micros();
+    float deltaT = ((float) (currT - prevT))/( 1.0e6 );
+    prevT = currT;
+    
+    //derivative
+    float dedt = (e-eprev)/(deltaT);
+
+    // integral
+    eintegral = eintegral + e*deltaT;
+
+    //control signal
+    float u = kp*e + kd*dedt + ki*eintegral;
+
+    int pwm = (int)fabs(-u+90);
+
+    //if(pwm < 93 )
+    //{
+    //  pwr = pwr-4;
+    //}
+
+//    if(pwr > 91 )
+//    {
+//      pwr = 91;
+//    }
+
+//    if(pwr < 85 )
+//    {
+//      pwr = 85;
+//    }
+
+//    if(e<0.05 || e>-0.05){
+//      pwr = 97;
 //    }
     
-    // motor direction
-    int dir = 1;
-    if(u<0){
-    dir = -1;
-    }
-    
-    // signal the motor
-    runMotor(dir,pwm,PWM,EN_A,EN_B);
-  
-		// store previous error
-		eprev = e;
 
-        
-  // Output
+    //SEND PWM
    
-    //Serial.print(millis());
+    
+    ESC.write(pwm);
+
+    // store previous error
+    eprev = e;
+
+    //Serial.print(kp,4);
     //Serial.print(", ");
-		
-		Serial.print(roll);
-		Serial.print(",");
-    //Serial.print(roll);
-    //Serial.print(",");
-    //Serial.println(vel);
-    //Serial.print(", ");
-    Serial.print(kp, 4);
+    Serial.print("IMU deg: ");
+    Serial.print(pos*10+50);
     Serial.print(", ");
-    //Serial.print(ki, 4);
-    //Serial.print(",");
-    //Serial.println(e);
-		//Serial.print(",");
-		Serial.println(pwm);
-		
-		
-		}
+    //Serial.print(millis());
+//    Serial.print("Error: ");
+//    Serial.print(e);
+//    Serial.print(", ");
+//    Serial.print("U: ");
+//    Serial.print(u);
+//    Serial.print(", ");
+    Serial.print("PWM: ");
+    Serial.println(pwm);
+    
+    }
   }
+}
 
 void actuate()
 {
   actuator.write(50);
+   
+    
 }
 
 void disengage()
 {
   actuator.write(90);
- 
-}
-
-void runMotor(int dir, int pwmVal, int pwmPin, int in1, int in2)
-{
-    // Set the direction of the motor
-    if(dir == 1){
-    digitalWrite(in1, HIGH);
-    digitalWrite(in2, LOW);
-    }
-    else if(dir == -1){
-    digitalWrite(in1, LOW);
-    digitalWrite(in2, HIGH);
-    }
-    else{
-    digitalWrite(in1, LOW);
-    digitalWrite(in2, LOW);
-    }
-
-    // Output PWM signal to motor driver
-    analogWrite(pwmPin, pwmVal);  
+  ESC.write(90);
+  
+  
 }
